2b1b039462a132e21a9dec09a8026c8612714177
caihw <35018234+caihw@users.noreply.github.com>
2020-12-06 10:26:22 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+{{#if element}}
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+{{/if}}
+{{#if antdesign}}
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
+{{/if}}
 Vue.config.productionTip = false;
 
 new Vue({
