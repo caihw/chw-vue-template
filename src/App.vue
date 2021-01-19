eeacb0d4172d647bf982d0ac526a83a3c61bf615
caihw <2506495452@qq.com>
2021-01-19 15:50:17 +0800

    修改app.vue 样式文件

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,27 @@
 </template>
 {{#if sass}}
 <style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+
+
+#nav
+  padding: 25px
+
+a
+  font-weight: bold
+  color: #2c3e50
+
+a.router-link-exact-active
+  color: #42b983
+</style>
 {{/if}}
 {{#if less}}
 <style lang="less">
-{{/if}}
-{{#if stylus}}
-<style lang="stylus">
-{{/if}}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -39,3 +53,57 @@ a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+{{/if}}
+{{#if stylus}}
+<style lang="stylus">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 25px;
+
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+
+}
+
+a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+{{/if}}
+{{#if nopreprocessor}}
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 25px;
+
+}
+
+a {
+  font-weight: bold;
+  color: #2c3e50;
+
+}
+
+a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+{{/if}}
+
